83cc3975393d928511769dc0e3fb49cbc14a96bc
i32ropie <i32ropie@uco.es>
2018-09-20 10:51:57 +0200

    Actualizado un poquito

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ with open('preguntas.json') as f: preguntas = json.load(f)
 @bot.message_handler(commands = ['start', 'ayuda', 'help'])
 def reglas_handler(m):
     cid = m.chat.id
-    mensaje = "Bienvenido al bot, para empezar a jugar haz click en /jugar y recibirás una carta aleatoria que tendrá 6 preguntas, cada una de una categoría distinta. Las categorías son:\n- *VC*: Vida cotidiana\n- *I*: Imaginario\n- *S*: Sexo\n- *C*: Cultura\n- *G*: Gore\n- *FP*: Filosofía y Progreso.\n\nLas reglas para jugar puedes inventártelas pero unas básicas serían que uno de los jugadores 'sacara' una carta e hiciera la pregunta que quiera de las que le han salido, entonces todos los presentes responden y la persona que sacó la carta decide qué respuesta es la que más le ha gustado y las demás personas beben un trago.\n\nLo importante es pasarlo bien, a si que juega como te de la gana.\n\nBot desarrollado por @Edurolp"
+    mensaje = "Bienvenido al bot. Para empezar a jugar haz click en /jugar y recibirás una carta aleatoria que tendrá 6 preguntas, cada una de una categoría distinta. Las categorías son:\n- *VC*: Vida cotidiana\n- *I*: Imaginario\n- *S*: Sexo\n- *C*: Cultura\n- *G*: Gore\n- *FP*: Filosofía y Progreso.\n\nLas reglas para jugar puedes inventártelas pero unas básicas serían que uno de los jugadores 'sacara' una carta e hiciera la pregunta que quiera de las que le han salido, entonces todos los presentes responden y la persona que sacó la carta decide qué respuesta es la que más le ha gustado y las demás personas beben un trago.\n\nLo importante es pasarlo bien, a si que juega como te de la gana.\n\nBot desarrollado por @Edurolp.\n\nOtros bots míos:\n\t- @LoL_bot\n\t- @CuteCatBot\n\t- @PugLove_bot"
     bot.send_message(cid, mensaje, parse_mode = "Markdown")
 
 @bot.message_handler(commands = ['jugar'])
@@ -19,7 +19,7 @@ def jugar_handler(m):
     p = random.choice(preguntas)
     teclado = types.InlineKeyboardMarkup()
     teclado.add(types.InlineKeyboardButton("Sacar otra carta", callback_data="carta"))
-    mensaje = "Estas son tus preguntas:\n\n- 👸🏻 *VC* - _{}_\n- 💭 *I*: _{}_\n- 😜 *S*: _{}_\n- 👤 *C*: _{}_\n- 💣 *G*: _{}_\n- 📚 *FP*: _{}_\n\nHaz click en *'Sacar otra carta'* para que el siguiente jugador vea nuevas preguntas."
+    mensaje = "Estas son tus preguntas:\n\n- 👸🏻 *VC*: _{}_\n- 💭 *I*: _{}_\n- 😜 *S*: _{}_\n- 👤 *C*: _{}_\n- 💣 *G*: _{}_\n- 📚 *FP*: _{}_\n\nHaz click en *'Sacar otra carta'* para que el siguiente jugador vea nuevas preguntas."
     bot.send_message(cid, mensaje.format(p['vida_cotidiana'],
                                          p['imaginario'],
                                          p['sexo'],
@@ -34,7 +34,7 @@ def carta_hander(call):
     p = random.choice(preguntas)
     teclado = types.InlineKeyboardMarkup()
     teclado.add(types.InlineKeyboardButton("Sacar otra carta", callback_data="carta"))
-    mensaje = "Estas son tus preguntas:\n\n- 👸🏻*VC* - _{}_\n- 💭 *I*: _{}_\n- 😜 *S*: _{}_\n- 👤 *C*: _{}_\n- 💣 *G*: _{}_\n- 📚 *FP*: _{}_\n\nHaz click en *'Sacar otra carta'* para que el siguiente jugador vea nuevas preguntas."
+    mensaje = "Estas son tus preguntas:\n\n- 👸🏻*VC*: _{}_\n- 💭 *I*: _{}_\n- 😜 *S*: _{}_\n- 👤 *C*: _{}_\n- 💣 *G*: _{}_\n- 📚 *FP*: _{}_\n\nHaz click en *'Sacar otra carta'* para que el siguiente jugador vea nuevas preguntas."
     bot.edit_message_text(mensaje.format(p['vida_cotidiana'],
                                              p['imaginario'],
                                              p['sexo'],
